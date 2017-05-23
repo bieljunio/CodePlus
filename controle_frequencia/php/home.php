b3a3dc6ff0b84f0106dc5e35c84bef85db0b37be
bieljunio <gabper@alunos.utfpr.edu.br>
2017-05-23 14:00:34 -0300

    Atualização do arquivo

--- controle_frequencia/php/home.php
+++ controle_frequencia/php/home.php
@@ -1,14 +1,40 @@
 <?php
     require 'validationlogin.php';
+    require 'dict.inc.php';
 ?>
 
 <!DOCTYPE html>
 <html>
+
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta charset="utf-8"/>
         <title>Home</title>
     </head>
+    
+    <?php 
+	if (isset($_GET['msg'])) {
+		switch ($_GET['msg']) {
+			case md5('ENTRY_SUCCESS'):
+				$S_msg = $dict[$_GET['msg']];
+				break;
+			case md5('ENTRY_FAIL'):
+				$S_msg = $dict[$_GET['msg']];
+				break;
+			default:
+				$S_msg = 'Operação já efetuada!';
+				break;
+		}
+	?>
+	<script type="text/javascript">
+		window.onload = function() {
+	        alert('<?php echo $S_msg; ?>');
+	    }
+	</script>
+	<?php 
+	}
+	?>
+    
     <body>
     <?php 
     $S_in = md5('entry');
@@ -16,8 +42,10 @@
     echo "<p><a href='registerPoint.php?in={$S_in}'>Registrar Entrada</a></p>";
     echo "<p><a href='registerPoint.php?in={$S_out}'>Registrar Saída</a></p>";
 	?>
+	
         WELCOME!
         <br><br>
         <h3><a href="logout.php">LOGOUT</a></h3>
+        
     </body>
 </html>
