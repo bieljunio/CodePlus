1961cf83e6b433c6101e6c83a163e35eb6952251
bieljunio <gabper@alunos.utfpr.edu.br>
2017-05-23 14:30:48 -0300

    Inserção de índices dict

--- controle_frequencia/php/dict.inc.php
+++ controle_frequencia/php/dict.inc.php
@@ -2,4 +2,6 @@
 $dict[md5('LOGIN_FAULT')] = 'Dados de acesso incorretos ou inválidos!';
 $dict[md5('ENTRY_SUCCESS')] = 'Registro inserido com sucesso!';
 $dict[md5('ENTRY_FAIL')] = 'Registro já foi inserido na data de hoje!';
+$dict[md5('EXIT_SUCCESS')] = 'Registro inserido com sucesso!';
+$dict[md5('EXIT_FAIL')] = 'Registro já foi inserido na data de hoje!';
 ?>
