3dacce1adef11e5186b22a6a77c76b74f29734ba
bieljunio <gabper@alunos.utfpr.edu.br>
2017-05-22 21:37:46 -0300

    Botão de Entrada completo

--- controle_frequencia/php/registerPoint.php
+++ controle_frequencia/php/registerPoint.php
@@ -3,6 +3,7 @@ require 'bd.php';
 require 'validationLogin.php';
 require 'funcoes.php';
 
+//define a data e hora atual
 date_default_timezone_set('America/Sao_Paulo');
 $date = date('Y-m-d');
 $tm_date = date('H:i:s');
@@ -11,9 +12,9 @@ if(isset($_GET['in'])){
 	switch ($_GET['in']){
 		case md5('entry'):
 			//faz consulta no banco para ver se já houve uma inserção nesta data
-			$EntryVerification = registrar_entrada($_SESSION['user'], $date, $tm_Entrada);
+			$EntryVerification = registrar_entrada($_SESSION['user'], $date, $tm_date);
 			if($EntryVerification == 1){
-				header("Location: home.php");
+				header("Location: ../index.php");
 			} else {
 				//retorna a mensagem de já haver um ponto registrado
 				echo "Erro";
