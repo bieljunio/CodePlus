091cc14a45d4f15f348be3d173d856eb9f8fcfb4
bieljunio <gabper@alunos.utfpr.edu.br>
2017-05-24 16:21:15 -0300

    Atualização

--- controle_frequencia/php/home.php
+++ controle_frequencia/php/home.php
@@ -7,51 +7,20 @@
 <html>
 
     <head>
+    	<title>Home</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta charset="utf-8"/>
-        <title>Home</title>
+        <script src="../javascript/jquery-3.2.1.min.js"></script>
+        <script src="../javascript/jquery.js"></script>
+        
     </head>
-    
-    <?php 
-	if (isset($_GET['msg'])) {
-		switch ($_GET['msg']) {
-			case md5('ENTRY_SUCCESS'):
-				$S_msg = $dict[$_GET['msg']];
-				break;
-			case md5('ENTRY_FAIL'):
-				$S_msg = $dict[$_GET['msg']];
-				break;
-			case md5('EXIT_SUCCESS'):
-				$S_msg = $dict[$_GET['msg']];
-				break;
-			case md5('EXIT_FAIL'):
-				$S_msg = $dict[$_GET['msg']];
-				break;
-			default:
-				$S_msg = 'Operação já efetuada!';
-				break;
-		}
-	?>
-	<script type="text/javascript">
-		window.onload = function() {
-	        alert('<?php echo $S_msg; ?>');
-	    }
-	</script>
-	<?php 
-	}
-	?>
-    
     <body>
-    <?php 
-    $S_in = md5('entry');
-    $S_out = md5('exit');
-    echo "<p><a href='registerPoint.php?in={$S_in}'>Registrar Entrada</a></p>";
-    echo "<p><a href='registerPoint.php?in={$S_out}'>Registrar Saída</a></p>";
-	?>
-	
-        WELCOME!
+		<p>WELCOME!</p>
         <br><br>
+       	<p><a href="" onclick="return registerEntry();">Registrar Entrada</a></p>
+        <p><a href="" onclick="return registerExit();">Registrar Saída</a></p>
         <h3><a href="logout.php">LOGOUT</a></h3>
+        
         
     </body>
 </html>
