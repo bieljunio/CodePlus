780796d63277984c9c7c7ccd556eba223f6708fb
bieljunio <gabper@alunos.utfpr.edu.br>
2017-05-24 16:20:29 -0300

    Atualização

--- controle_frequencia/php/registerPoint.php
+++ controle_frequencia/php/registerPoint.php
@@ -1,47 +1,47 @@
 <?php
-
+require 'validationlogin.php';
 require 'bd.php';
-require 'validationLogin.php';
 require 'funcoes.php';
 
-//define a data e hora atual
-date_default_timezone_set('America/Sao_Paulo');
-$date = date('Y-m-d');
-$tm_date = date('H:i:s');
-//recebe os parâmetros passados pela page home.php
-if(isset($_GET['in'])){
-	switch ($_GET['in']){
-		case md5('entry'):
-			//faz consulta no banco para ver se já houve uma inserção nesta data
-			$EntryVerification = registrar_entrada($_SESSION['user'], $date, $tm_date);
-			if($EntryVerification == 1){
-				//envia mensagem de sucesso
-				$S_msg = md5('ENTRY_SUCCESS');
-				header("Location: home.php?msg={$S_msg}");
-			} else {
-				//retorna a mensagem de já haver um ponto registrado
-				$S_msg = md5('ENTRY_FAIL');
-				header("Location: home.php?msg={$S_msg}");
-			}
-			
-			break;
-		case md5('exit'):
-			//faz consulta no banco para ver se já ouve uma inserção de saída
-			$EntryVerification = registrar_saida($_SESSION['user'], $date, $tm_date);
-			//Retorna 1 se tiver uma inserção
-			if($EntryVerification == 1){
-				//envia mensagem de sucesso
-				$S_msg = md5('EXIT_SUCCESS');
-				header("Location: home.php?msg={$S_msg}");
-			} else {
-				//envia mensagem de ponto já inserido
-				$S_msg = md5('EXIT_FAIL');
-				header("Location: home.php?msg={$S_msg}");
-			}
-			break;
-		default:
-			
+//Define a função para o botão entrada
+function buttonEntry($userCPF, $dateNow, $tm_dateNow){
+	//faz consulta no banco de dados e ve se já houve inserção na data, caso não tenha feito, insere e retorna 1
+	$EntryVerification = registrar_entrada($userCPF, $dateNow, $tm_dateNow);
+	if($EntryVerification == 1){
+		$msg = 'Registro de entrada efetuada com sucesso!';
+	} else{
+		$msg = 'Registro já efetuado na data de hoje!';
 	}
+	return $msg;
 }
+
+//Define a função para o botão saída
+function buttonExit($userCPF, $dateNow, $tm_dateNow){
+	//faz a consulta e insere a saida e retorna 1, caso haja uma inserção já feita na data retorna erro
+	$EntryVerification = registrar_saida($userCPF, $dateNow, $tm_dateNow);
+	if($EntryVerification == 1){
+		$msg = 'Registro de saída efetuada com sucesso!';
+	} else{
+		$msg = 'Registro já efetuado na data de hoje!';
+	}
+	return $msg;
+}
+
+//executa o botão registrar entrada
+function buttonResult(){
+	//define a data e hora atual
+	date_default_timezone_set('America/Sao_Paulo');
+	$date = date('Y-m-d');
+	$tm_date = date('H:i:s');
+	
+	if(isset($_GET['entry'])){
+	$msgResult = buttonEntry($_SESSION['user'], $date, $tm_date);
+	} else if(isset ($_GET['exit'])) {
+	$msgResult = buttonExit($_SESSION['user'], $date, $tm_date);
+	}
+	echo $msgResult;
+}
+
+buttonResult();
 
 ?>
