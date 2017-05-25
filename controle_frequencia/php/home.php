0cbabf5b0f6a63d22e3a3217d7206f785dd35221
bieljunio <gabper@alunos.utfpr.edu.br>
2017-05-25 15:51:31 -0300

    Atualização final da página home

--- controle_frequencia/php/home.php
+++ controle_frequencia/php/home.php
@@ -1,63 +1,74 @@
-
 <?php
     require 'validationlogin.php';
     require_once 'dict.inc.php';
 ?>
-
 <!DOCTYPE html>
-<html>
-    <head>
-        
-    	<title>Home</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta charset="utf-8"/>
-        <script src="../javascript/jquery-3.2.1.min.js"></script>
-        <script src="../javascript/jquery.js"></script>
-
-
-        <?php
-        if (isset($_GET['msg'])) {
-            switch ($_GET['msg']) {
-                case md5('SENHA_FAULT'):
-                    $S_msg = $senha_fault[$_GET['msg']];
-                    break;
-                default:
-                    $S_msg = 'A senha inserida está incorreta.';
-                    break;
-            }
-            
-        ?>
-        <script type="text/javascript">
-            window.onload = function() {
-                alert('<?php echo $S_msg; ?>');
-            }
-        </script>
-        <?php
-        }
-        ?>
-
-    </head>
-	
-    <body>
+<html lang="pt_br">
     
-		<p>WELCOME!</p>
-        <br><br>
-       	<p><a href="" onclick="return registerEntry();">Registrar Entrada</a></p>
-        <p><a href="" onclick="return registerExit();">Registrar Saída</a></p>
+    <head>
+    	<title>Code Plus - Home</title>
+    	<!-- inclusões iniciais do arquivo html -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- fontes do google -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Pacifico|Roboto+Slab:400,700" 
+        rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="../css/layout.css">
+        <!-- Inserção de jquery -->
+        <script src="../javascript/jquery-3.2.1.min.js"></script>
+        <!--  Inserção de funções para os botões -->
+        <script src="../javascript/jquery.js"></script>
+    </head>
+
+    <body>
+        <header>
+            
+            <section class="logomarca">
+                <a href="home.php">
+                    <img class="logo" src="../img/logo.png" alt="logo">
+                </a>
+            </section>
+            
+            <section>
+                <nav id="registros">
+                    <ul>
+                        <li><a href="" onclick="return registerEntry();">Registrar Entrada</a></li>
+                        <li><a href="" onclick="return registerExit();">Registrar Saída</a></li>
+                    </ul>
+                </nav>
+            </section>
+            
+            <section class="box">
+                <div class="dropdown">
+                    <img class="seta" src="../img/seta.png" alt="seta" />
+                    <img class="redondo" src="../img/perfil.png"   alt="Foto Perfil" />
+                    <div class="dropdown_content">
+                        <a class="ac_perfil" href="#">Acessar perfil</a>
+                        <a class="al_senha" href="#">Alterar senha</a>
+                        <a class="sair" href="logout.php">Sair</a>
+                    </div>
+                </div>
+            </section>
+            
+       </header>
+            
+        <section>
+            
+            <nav id="menu">
+                <ul>
+                    <li><a href="#">FUNCIONÁRIOS</a></li>
+                    <li><a href="#">CONSULTAR FREQUÊNCIA</a></li>
+                    <li><a href="#">NOVO CADASTRO</a></li>
+                </ul>
+            </nav>
         
+        </section>
         
+        <!--FAÇA O CONTEUDO DO CODIGO A PARTIR DESSE PONTO-->
         
-        <h4>Alterar Senha:</h4> 
-          <form name="alterar_senha" id="alterar_senha" method="post" action="alterar_senha.php">
-              <input type="password" id="old_password" name="old_password" placeholder="Senha Antiga"></input><br><br>
-              <input type="password" id="new_password" name="new_password" placeholder="Senha Nova"></input><br><br>
-              <input type="password" id="confirm_new_password" name="confirm_new_password" placeholder="Confirme senha Nova"></input><br><br>
-              <br>
-              <button id="confirmar" type="submit" name="confirmar">Confirmar</button>
-          </form>
+        <section class="cont">
+        <!--INSIRA OS DADOS AQUI-->
+        </section>
         
-        <br><br>
-        <h3><a href="logout.php">LOGOUT</a></h3>
-        <script type="text/javascript" src="../javascript/alterasenha.js"></script>
     </body>
 </html>
