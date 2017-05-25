c72d79fe26ffaabddfa4ddb809b05c77656479a5
JanainaLudwig <janainaludwig@alunos.utfpr.edu.br>
2017-05-25 17:29:13 -0300

    Atualizações

---
+++ alterar_senha.php
@@ -0,0 +1,60 @@
+<?php
+
+require 'bd.php';
+require 'validationlogin.php';
+require 'funcoes.php';
+
+$S_SenhaAntiga = filter_input(INPUT_POST, 'old_password');
+$S_SenhaNova = filter_input(INPUT_POST, 'new_password');
+$S_ConfirmacaoSenhaNova = filter_input(INPUT_POST, 'confirm_new_password');
+
+$S_SenhaAntigaHash = retorna_senha($_SESSION['email']);
+
+$I_cpf = $_SESSION['user'];
+
+if($S_SenhaNova === $S_ConfirmacaoSenhaNova){	
+
+	if (password_verify($S_SenhaAntiga, $S_SenhaAntigaHash)) {
+
+		$S_SenhaNovaHash = password_hash($S_SenhaNova, PASSWORD_DEFAULT);
+
+		$sql = <<<HEREDOC
+		    UPDATE login
+			SET senha = '$S_SenhaNovaHash'
+			WHERE cpf = '$I_cpf'
+HEREDOC;
+
+		pg_query($sql);
+
+		date_default_timezone_set('America/Sao_Paulo');
+		$datahora = date('d-m-Y H:i:s');
+		
+
+		$slqlog = <<<HEREDOC
+			INSERT INTO log_alteracao (data_hora, tabela, campo, registro_antigo, cpf_alterado, cpf_responsavel)
+			VALUES ('$datahora', 'login', 'senha', '$S_SenhaAntigaHash', '$I_cpf', '$I_cpf')
+HEREDOC;
+
+		pg_query($slqlog);
+		?>
+
+		<script type="text/javascript">
+			alert('Senha alterada com sucesso');
+			window.location='logout.php';
+		</script>
+
+		<?php
+
+
+	} else {
+		$S_senhafault = md5('SENHA_FAULT');
+		header("Location: alteracaoSenha.php?msg={$S_senhafault}");
+
+	}
+} else {
+	header("Location: alteracaoSenha.php");
+}
+
+
+
+?>
---
+++ home.php
@@ -0,0 +1,76 @@
+<?php
+    require 'validationlogin.php';
+    require_once 'dict.inc.php';
+?>
+<!DOCTYPE html>
+<html lang="pt_br">
+    
+    <head>
+        <title>Code Plus - Home</title>
+        <!-- Favicon -->
+        <link rel="icon" href="../img/favicon.png" type="image/png">
+        <!-- inclusões iniciais do arquivo html -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- fontes do google -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Pacifico|Roboto+Slab:400,700" 
+        rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="../css/layout.css">
+        <!-- Inserção de jquery -->
+        <script src="../javascript/jquery-3.2.1.min.js"></script>
+        <!--  Inserção de funções para os botões -->
+        <script src="../javascript/jquery.js"></script>
+    </head>
+
+    <body>
+        <header>
+            
+            <section class="logomarca">
+                <a href="home.php">
+                    <img class="logo" src="../img/logo.png" alt="logo">
+                </a>
+            </section>
+            
+            <section>
+                <nav id="registros">
+                    <ul>
+                        <li><a href="" onclick="return registerEntry();">Registrar Entrada</a></li>
+                        <li><a href="" onclick="return registerExit();">Registrar Saída</a></li>
+                    </ul>
+                </nav>
+            </section>
+            
+            <section class="box">
+                <div class="dropdown">
+                    <img class="seta" src="../img/seta.png" alt="seta" />
+                    <img class="redondo" src="../img/perfil.png"   alt="Foto Perfil" />
+                    <div class="dropdown_content">
+                        <a class="ac_perfil" href="#">Acessar perfil</a>
+                        <a class="al_senha" href="#">Alterar senha</a>
+                        <a class="sair" href="logout.php">Sair</a>
+                    </div>
+                </div>
+            </section>
+            
+       </header>
+            
+        <section>
+            
+            <nav id="menu">
+                <ul>
+                    <li><a href="#">FUNCIONÁRIOS</a></li>
+                    <li><a href="#">CONSULTAR FREQUÊNCIA</a></li>
+                    <li><a href="#">NOVO CADASTRO</a></li>
+                </ul>
+            </nav>
+        
+        </section>
+        
+        <!--FAÇA O CONTEUDO DO CODIGO A PARTIR DESSE PONTO-->
+        
+        <section class="cont">
+        <!--INSIRA OS DADOS AQUI-->
+        </section>
+        
+    </body>
+</html>
