a4c49562f269b68b5e68cb4bbd11fb7dda3d9e16
bieljunio <gabper@alunos.utfpr.edu.br>
2017-05-31 14:38:34 -0300

    Atualização de campos

--- controle_frequencia/php/passCadastration.php
+++ controle_frequencia/php/passCadastration.php
@@ -3,12 +3,13 @@
 	$S_email = filter_input(INPUT_POST, 'email');
 	$S_password = filter_input(INPUT_POST, 'password');
 	$S_passwordConfirm = filter_input(INPUT_POST, 'passwordConfirm');
+	$S_email = strtoupper($S_email);
 
 	//Garante que o e-mail recebido pelo navegador é igual ao e-mail do usuário cadastrado
 	if(isset($_GET['e']) == md5($S_email)){
 		//verifica se já houve acesso no sistema.
 		$ultimoAcesso = pg_query("SELECT ultimo_acesso FROM login WHERE email = '$S_email'");
-		if($ultimoAcesso == NULL){
+		if(!$ultimoAcesso){
 			if($password === $passwordConfirm){
 				pg_query("UPDATE login SET senha = '$S_password' WHERE email = '$S_email'");
 			} else {
