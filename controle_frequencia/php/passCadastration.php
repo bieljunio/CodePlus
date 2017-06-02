21daadca9fb67594ef3c58abf5c85d628aaa728a
Biel Junio <gabper@alunos.utfpr.edu.br>
2017-06-02 11:52:10 -0300

    Ajustes

--- controle_frequencia/php/passCadastration.php
+++ controle_frequencia/php/passCadastration.php
@@ -9,12 +9,10 @@
 	if($e == md5($S_email)){
 		//Passa o e-mail para maiúsculo para ser enviado para o banco
 		$S_email = strtoupper($S_email);
-		echo "$S_email";
 		//verifica se já houve acesso no sistema.
 		$ultimoAcesso = pg_query("SELECT * FROM login WHERE email = '$S_email'");
 		if(pg_field_is_null($ultimoAcesso, 0, "ultimo_acesso") == 1){
 			if($S_password == $S_passwordConfirm){
-				echo "IF 3";
 				$S_senha = password_hash($S_password, PASSWORD_DEFAULT);
 				pg_query("UPDATE login SET senha = '$S_senha' WHERE email = '$S_email'");
 				echo "Senha cadastrada com sucesso";
