7081f06f55a63314600a45570499e754c4b2e321
Biel Junio <gabper@alunos.utfpr.edu.br>
2017-06-05 21:23:35 -0300

    Adição de função para busca em AJAX

--- controle_frequencia/javascript/jquery.js
+++ controle_frequencia/javascript/jquery.js
@@ -26,3 +26,18 @@ function registerExit(){
 	});
 	return false;
 }
+
+function search(){
+	var cp = new cpaint();
+	cp.set_transfer_mode("POST");
+	cp.set_response_type("TEXT");
+	
+	var name = document.getElementsByName("nameFilter")[0].value;
+	if(name != ""){
+		cp.call("buscaColaborador.php", name, tableConsult);
+	}
+}
+
+function tableConsult(Retorno){
+	
+}
