88644c58ff2f065e067ab4c7a3d06c1f5ca8700d
Biel Junio <gabper@alunos.utfpr.edu.br>
2017-06-06 00:16:43 -0300

    index do módulo de busca

--- controle_frequencia/php/colaborador.php
+++ controle_frequencia/php/colaborador.php
@@ -3,3 +3,19 @@
 	require 'validationlogin.php';
 	require 'funcoes.php';
  ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Busca de cadastro</title>
+	<meta charset="utf-8" />
+	<!--  JavaScript, Jquery -->
+	<script src="../javascript/jquery.js" type="text/javascript"></script>
+	<script src="../javascript/jquery-3.2.1.min.js" type="text/javascript"></script>
+	<!-- CPAINT 2 framework -->
+	<script src="../javascript/cpaint2/cpaint2.inc.compressed.js" type="text/javascript"></script>
+</head>
+<body>
+	<p><input type="text" name="nameFilter" /></p>
+	<p><a href="" onclick="search();">Pesquisar</a></p>
+</body>
+</html>
