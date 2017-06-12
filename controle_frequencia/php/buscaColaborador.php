d10d3cafe6394ac31df1e36aaab29ff0ebf48dc1
JanainaLudwig <janainaludwig@alunos.utfpr.edu.br>
2017-06-12 18:12:25 -0300

    Módulo de edição de dados cadastrais
    Alteração na busca de colaborados

--- controle_frequencia/php/buscaColaborador.php
+++ controle_frequencia/php/buscaColaborador.php
@@ -3,28 +3,37 @@
 	header("Cache-Control: post-check=0, pre-check=0", false);
 	header("Pragma: no-cache");
 	require 'bd.php';
+	require 'validationlogin.php';
 	$name = filter_input(INPUT_POST, 'nameFilter');
 	$name = strtoupper(strval($name));
 	$sql = <<<HEREDOC
-		SELECT F.NOME, S.NOME AS SETOR
-		FROM FUNCIONARIO F INNER JOIN SETOR S
-		ON F.ID_SETOR = S.ID_SETOR
-		WHERE F.NOME LIKE '%$name%'
+		SELECT F.CPF,
+	   F.NOME,
+	   S.NOME AS SETOR
+	FROM FUNCIONARIO F
+		INNER JOIN SETOR S
+			ON F.ID_SETOR = S.ID_SETOR
+			WHERE F.NOME LIKE '%$name%'
 HEREDOC;
 	$sql = pg_query($sql);
 	$rows = pg_num_rows($sql);
 	if($rows){
+		unset($_SESSION['busca']);
+		$i = 0;
 		while($linha = pg_fetch_array($sql)){
-			$nome =  $linha[0];
-			$setor = $linha[1];
+			//$cpf = md5($linha[0]);
+			$_SESSION['busca'][$i] = $linha[0];
+			$nome =  $linha[1];
+			$setor = $linha[2];
 			echo "<tbody>
 					<tr>
 						<td id='nome'>$nome</td>
 						<td id='setor'>$setor</td>
 						<td id='ferramentas'><a href=''><i class='fa fa-user-o' aria-hidden='true'></i></a>
-							<a href=''><i class='fa fa-bars' aria-hidden='true'></i></a></td>
+							<a target='_blank' href='viewperfil.php?c=$i'><i class='fa fa-bars' aria-hidden='true'></i></a></td>
 					</tr>
 				</tbody>";
+				$i++;
 		}
 	}
 ?>
